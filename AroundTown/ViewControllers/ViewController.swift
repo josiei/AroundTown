5785f7e3ecc626037f48a71402fa23f4f1ef58fd
Josie Icaza <jocelyn.icaza@gmail.com>
2022-12-29 15:03:22 -0800

    - Set up basic HeaderStack layout

--- AroundTown/ViewControllers/ViewController.swift
+++ AroundTown/ViewControllers/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var venueModel = VenueModel()
     var venues = [Venue]()
     let containerView = UIStackView()
+    let locationLabel = UILabel()
     
     
     
@@ -27,20 +28,19 @@ class ViewController: UIViewController {
         
         //Set up layout
         setUpParentView()
+        setUpHeader(view: containerView)
         
-        //Set the default text
-        //locationLabel.text = locationModel.userLocation
         
         //Subscribe VC to notification when LocationModel changes userLocation property
-//        NotificationCenter.default.addObserver(self, selector: #selector(locationChanged(_:)), name: NSNotification.Name("LocationChanged"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(locationChanged(_:)), name: NSNotification.Name("LocationChanged"), object: nil)
 
         
     }
     
     //Change the locationLabel text when userLocation gets updated
-//    @objc func locationChanged(_ notification: Notification){
-//        locationLabel.text = locationModel.userLocation
-//    }
+    @objc func locationChanged(_ notification: Notification){
+        locationLabel.text = locationModel.userLocation
+    }
     
     func setUpParentView(){
         view.addSubview(containerView)
@@ -60,7 +60,40 @@ class ViewController: UIViewController {
         containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
         containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
         
+        //Set Background Color
+        containerView.backgroundColor = .white
+        
     }
+    
+    func setUpHeader(view: UIStackView){
+        //create container
+        let verticalStack = UIStackView()
+        verticalStack.axis = .vertical
+        verticalStack.spacing = 3
+        
+        //add padding
+        verticalStack.layoutMargins = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 0)
+        verticalStack.isLayoutMarginsRelativeArrangement = true
+        
+        //Set up Labels
+        let findVenuesLabel = UILabel()
+        findVenuesLabel.text = "Find Venues in"
+        locationLabel.text = locationModel.userLocation
+        
+        //Add label to vertical stack
+        verticalStack.addArrangedSubview(findVenuesLabel)
+        verticalStack.addArrangedSubview(locationLabel)
+        
+        //add to parent view
+        view.addArrangedSubview(verticalStack)
+        
+        
+    }
+
+    // TODO:
+//    func setUpSubViewsProportionally() {
+//
+//    }
     
 
 
