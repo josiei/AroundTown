c17e8d02e7672caf769bcc4e345e26212f2481d3
Josie Icaza <jocelyn.icaza@gmail.com>
2022-12-29 17:03:41 -0800

    - Added function to draw a circle for button background image and add to view

--- AroundTown/ViewControllers/ViewController.swift
+++ AroundTown/ViewControllers/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
         //Set up layout
         setUpParentView()
         setUpHeader(view: containerView)
+        createBaseButton(view: containerView)
         
         
         //Subscribe VC to notification when LocationModel changes userLocation property
@@ -66,12 +67,12 @@ class ViewController: UIViewController {
     }
     
     func setUpHeader(view: UIStackView){
-        //create container
+        //Create container
         let verticalStack = UIStackView()
         verticalStack.axis = .vertical
         verticalStack.spacing = 3
         
-        //add padding
+        //Add padding
         verticalStack.layoutMargins = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 0)
         verticalStack.isLayoutMarginsRelativeArrangement = true
         
@@ -84,10 +85,44 @@ class ViewController: UIViewController {
         verticalStack.addArrangedSubview(findVenuesLabel)
         verticalStack.addArrangedSubview(locationLabel)
         
-        //add to parent view
+        //Add stack to parent view
         view.addArrangedSubview(verticalStack)
         
         
+    }
+    
+    func createBaseButton(view: UIView){
+        let button = UIButton(type: .system)
+        
+        //Set button's title and color
+        button.frame = CGRect(x: 50, y: 50, width: 65, height: 65)
+        button.setTitle("Test", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        
+        //Create a circular path for the outline of the button
+        let diameter = min(button.frame.width, button.frame.height)
+        let radius = diameter / 2.0
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: radius, y: radius), radius: radius - 5, startAngle: 0, endAngle: CGFloat.pi * 2.0, clockwise: true)
+        
+        //Create shapeLayer and set properties
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.black.cgColor
+        shapeLayer.lineWidth = 2.0
+        
+        //Create image object from shapeLayer
+        let imageSize = CGSize(width: diameter, height: diameter)
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0.0)
+        
+        //Can force unwrap as we just created context
+        shapeLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        //UIGraphicsEndImageContext()
+        
+        button.setBackgroundImage(image, for: .normal)
+        
+        view.addSubview(button)
     }
 
     // TODO:
