a78f040d3586b982d1b226f3234b57ad209304d0
Josie Icaza <jocelyn.icaza@gmail.com>
2023-01-09 00:07:28 -0800

    - Loaded venue's webpage into webView

--- AroundTown/ViewControllers/WebViewController.swift
+++ AroundTown/ViewControllers/WebViewController.swift
@@ -12,11 +12,53 @@ class WebViewController: UIViewController {
     
     var webView = WKWebView()
     var websiteUrl: String?
+    var spinner = UIActivityIndicatorView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         webView.navigationDelegate = self
+        setupWebView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //If there is a url
+        if websiteUrl != nil {
+            
+            print("WEEEEEB SITE URL", websiteUrl!)
+            
+            let url = URL(string: websiteUrl!)
+            
+            guard url != nil else {
+                print("Could not create URL object")
+                return
+            }
+            
+            //Create request object
+            let request = URLRequest(url: url!)
+            
+            spinner.alpha = 1
+            spinner.startAnimating()
+            
+            //Load to webView
+            webView.load(request)
+            
+        }
+        
+    }
+    
+    func setupWebView(){
+        view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
     }
     
 
@@ -24,5 +66,11 @@ class WebViewController: UIViewController {
 
 extension WebViewController: WKNavigationDelegate {
     
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        spinner.stopAnimating()
+        spinner.alpha = 0
+        
+    }
     
 }
