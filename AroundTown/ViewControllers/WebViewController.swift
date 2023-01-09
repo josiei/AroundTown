7794ef0623f0210e202f262563a7e1b19c71ccca
Josie Icaza <jocelyn.icaza@gmail.com>
2023-01-08 22:44:21 -0800

    - Created webVC class

---
+++ AroundTown/ViewControllers/WebViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WebViewController.swift
+//  AroundTown
+//
+//  Created by Jocelyn Icaza on 1/8/23.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    
+    var websiteUrl: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
